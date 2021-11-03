706f3b683c36d26d07e76d8e03d219ca70ac0885
Dmitry Diordychuk <cort@mail.ru>
2021-11-03 20:47:06 +0300

    ex01 done

--- ex/urls.py
+++ ex/urls.py
@@ -1,25 +1,21 @@
-"""d07 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.urls.exceptions import NoReverseMatch
-from .views import Articles, Home, Login
+from .views import \
+    Articles, \
+    Favourites, \
+    Home, \
+    Login, \
+    Publications, \
+    ArticleDetail, \
+    LogoutView
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('articles/', Articles.as_view(), name='articles'),
-    path('login/', Login.as_view(), name='login')
+    path('login/', Login.as_view(), name='login'),
+    path('publications/', Publications.as_view(), name='publications'),
+    path('detail/<str:pk>', ArticleDetail.as_view(), name='detail'),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('favourites/', Favourites.as_view(), name="favourites"),
 ]
--- ex/views.py
+++ ex/views.py
@@ -1,8 +1,9 @@
+from django.http import request
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, RedirectView
-from django.contrib.auth.views import LoginView
+from django.views.generic import ListView, RedirectView, DetailView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from .models import Article
+from .models import Article, UserFavouriteArticle
 from django.contrib import auth
 
 # Create your views here.
@@ -15,8 +16,35 @@ class Articles(ListView):
 
 
 class Login(LoginView):
-	def get(self, request, *args, **kwargs):
-		if request.user.is_authenticated:
-			auth.logout(request)
-			return redirect(reverse_lazy('home'))
-		return super().get(request, *args, **kwargs)
+	pass
+
+
+class Publications(ListView):
+	model = Article
+	template_name = 'ex/publication_list.html'
+
+	def get_queryset(self):
+		return Article.objects.filter(author=self.request.user)
+
+
+class ArticleDetail(DetailView):
+	model = Article
+	template = 'ex/article.html'
+	context_object_name = 'article'
+
+
+class Logout(LogoutView):
+	pass
+
+
+class Favourites(ListView):
+	model = Article
+	template_name = 'ex/favourite_list.html'
+
+	def get_queryset(self):
+		return (
+			UserFavouriteArticle
+				.objects
+				.filter(user=self.request.user)
+				.select_related('article')
+		)
